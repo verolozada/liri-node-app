1338f51316ceab0e71299f3bf23c110e6e4497ff
Veronica L <verolozada12@gmail.com>
2018-10-22 18:47:40 -0400

    first files created

---
+++ liri.js
@@ -0,0 +1,15 @@
+// Bands in Town API 173f2037f8ffa320da5b8b977724a13
+
+// OMDb request link http://www.omdbapi.com/?i=tt3896198&apikey=841bb874
+
+js
+require("dotenv").config();
+
+
+const requireSpotify = require('./keys')
+
+js
+  var spotify = new Spotify(keys.spotify);
+
+  
+
